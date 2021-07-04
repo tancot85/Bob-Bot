749d5510677321fd1a4fcec297a78957fba7b027
Tanmay Tete <Tanmay>
2021-07-04 18:13:36 +0530

    added rock paper scissors

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ from discord import member
 import time
 import os
 import reddit
+import random
 # from dotenv import load_dotenv
 # import random
 # from discord.ext import commands
@@ -18,6 +19,7 @@ client = discord.Client(intents=intents)
 RULES = "Gentlemen, welcome to Fight Club. \nThe first rule of Fight Club is: You do not talk about Fight Club. \nThe second rule of Fight Club is: You do not talk about Fight Club. \nThird rule of Fight Club: Someone yells \"Stop!\", goes limp, taps out, the fight is over. \nFourth rule: Only two guys to a fight. Fifth rule: One fight at a time, fellas. \nSixth rule: No shirts, no shoes. \nSeventh rule: Fights will go on as long as they have to. \nAnd the eighth and final rule: If this is your first night at Fight Club, you have to fight."
 REACTIONS = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
 # reactions = [":hand_splayed:", ":hand_fist:", ":v:"]
+RPS = ["🖐️", "✊", "✌️"]
 
 
 def createMsg(nxt):
@@ -27,6 +29,23 @@ def createMsg(nxt):
         msg = msg + str(i) + ". " + nxt[i] + "\n"
         emojis.append(REACTIONS[i])
     return msg, emojis
+
+
+def rps(a, b):
+    if a == b:
+        return 0
+    elif a == "🖐️" and b == "✊":
+        return 1
+    elif a == "✊" and b == "🖐️":
+        return -1
+    elif a == "✌️" and b == "🖐️":
+        return 1
+    elif a == "🖐️" and b == "✌️":
+        return -1
+    elif a == "✊" and b == "✌️":
+        return 1
+    elif a == "✌️" and b == "✊":
+        return -1
 
 
 @client.event
@@ -42,16 +61,24 @@ async def on_member_join(member):
     # await member.send(content=RULES)
     embedVar = discord.Embed(
         title="Gentlemen, welcome to Fight Club.", description="", color=0x0000FF)
-    embedVar.add_field(name="The first rule of Fight Club is:", value="You do not talk about Fight Club.", inline=False)
-    embedVar.add_field(name="The second rule of Fight Club is", value="You do not talk about Fight Club!", inline=False)
-    embedVar.add_field(name="Third rule of Fight Club", value="Someone yells \"Stop!\", goes limp, taps out, the fight is over", inline=False)
-    embedVar.add_field(name="Fourth rule", value="Only two guys to a fight", inline=False)
-    embedVar.add_field(name="Fifth rule", value="One fight at a time, fellas", inline=False)
-    embedVar.add_field(name="Sixth rule", value="No shirt, no shoes", inline=False)
-    embedVar.add_field(name="Seventh rule", value="Fights will go on as long as they have to", inline=False)
-    embedVar.add_field(name="The eighth and final rule", value="If this is your first night at Fight Club, you have to fight.", inline=False)
+    embedVar.add_field(name="The first rule of Fight Club is:",
+                       value="You do not talk about Fight Club.", inline=False)
+    embedVar.add_field(name="The second rule of Fight Club is",
+                       value="You do not talk about Fight Club!", inline=False)
+    embedVar.add_field(name="Third rule of Fight Club",
+                       value="Someone yells \"Stop!\", goes limp, taps out, the fight is over", inline=False)
+    embedVar.add_field(name="Fourth rule",
+                       value="Only two guys to a fight", inline=False)
+    embedVar.add_field(name="Fifth rule",
+                       value="One fight at a time, fellas", inline=False)
+    embedVar.add_field(name="Sixth rule",
+                       value="No shirt, no shoes", inline=False)
+    embedVar.add_field(
+        name="Seventh rule", value="Fights will go on as long as they have to", inline=False)
+    embedVar.add_field(name="The eighth and final rule",
+                       value="If this is your first night at Fight Club, you have to fight.", inline=False)
     await member.send(embed=embedVar)
-    
+
     # member.nick = "tylerDurden"
 
 
@@ -79,27 +106,25 @@ async def on_message(message):
         print(nxt)
         if len(nxt) == 2:
             rslash = nxt[1]
-            url,title = reddit.getNewPost(rslash)
+            url, title = reddit.getNewPost(rslash)
             await message.channel.send("From " + title+": ")
             await message.channel.send(url)
-        elif len(nxt) == 3 :
+        elif len(nxt) == 3:
             rslash = nxt[1]
-            if nxt[2] == "top" :
-                url,title = reddit.getTopPost(rslash)
+            if nxt[2] == "top":
+                url, title = reddit.getTopPost(rslash)
                 await message.channel.send("From " + title+" top post: ")
                 await message.channel.send(url)
             elif nxt[2] == "new":
-                url,title = reddit.getNewPost(rslash)
+                url, title = reddit.getNewPost(rslash)
                 await message.channel.send("From " + title+": ")
                 await message.channel.send(url)
             elif nxt[2] == "hot":
-                url,title = reddit.getNewPost(rslash)
+                url, title = reddit.getNewPost(rslash)
                 await message.channel.send("From " + title+" hotest post: ")
                 await message.channel.send(url)
             else:
                 await message.channel.send("error in command")
-
-
 
         # print(url)
         # await message.channel.send("From " + title+": ")
@@ -145,44 +170,33 @@ async def on_message(message):
                 winning_msg = winning_msg + i + ", "
             await message.channel.send(winning_msg)
 
-    if message.content.startswith(">play"):
-        _words = message.content.split(" ")
-        print("words", _words)
-        nxt = _words[1]
-        print(nxt)
-        if nxt == "rps":
-            print("sleeping")
-            time.sleep(5)
-            print("awake")
-            print('reacts:', message.reactions)
-            if(len(message.reactions) == 1):
-
-                print("gotcha")
-                user_r = message.reactions[0]
-                print(user_r)
-                # bot_r = reactions[random.randint(0,2)]
-                # message.add_react(bot_r)
-
-
-# def determine_winner(bot,user):
-#     if(bot == user):
-#         return 0
-
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('-hello there'):
-#         await message.channel.send('General Kenobi!')
+    if message.content.startswith(">rock paper scissor"):
+        _msg = await message.channel.send("React to this msg")
+        _id = _msg.id
+        for emo in RPS:
+            await _msg.add_reaction(emo)
+        time.sleep(5)
+        getmsg = await message.channel.fetch_message(_id)
+        rns = getmsg.reactions
+        print(rns)
+        count = 0
+        voted_reaction = ""
+        for react in rns:
+            if react.count > count:
+                count = react.count
+                voted_reaction = react.emoji
+        bot_reaction = random.choice(RPS)
+        print("bot reaction = " + bot_reaction)
+        await _msg.add_reaction(bot_reaction)
+        result = rps(voted_reaction, bot_reaction)
+        if result == 0:
+            await message.channel.send("the bot chose "+bot_reaction+" its a tie")
+        elif result == -1:
+            await message.channel.send("the bot chose "+bot_reaction + " you lose")
+        elif result == 1:
+            await message.channel.send("the bot chose "+bot_reaction + " you win")
+        print("out of the loop")
 
 
-# @client.command()
-# async def hello_there(ctx):
-#     await ctx.send("general Kenobi")
-
-
-# TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 client.run('ODQ4MjI3MTQ2MTI3MTE0MjUw.YLJjFg.qgIaCIi37dSGNk0n-DBaJBosXT8')
 # client.run(TOKEN)
